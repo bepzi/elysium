331a6d43e03ac8cea518be6e936cbadd507b13b5
bepzi <bepzi@bepzi.com>
2021-04-17 20:10:16 -0700

    Generate a sine wave

--- src/rust/src/processor.rs
+++ src/rust/src/processor.rs
@@ -3,13 +3,18 @@ use rand::rngs::ThreadRng;
 use rand::Rng;
 use std::ptr;
 
-const DEFAULT_SAMPLE_RATE: f64 = 41000.0;
-
 use crate::ffi;
+
+const DEFAULT_SAMPLE_RATE: f64 = 41000.0;
+const DEFAULT_FREQUENCY: f64 = 440.0;
 
 pub struct ElysiumAudioProcessor {
     rng: ThreadRng,
     sample_rate: f64,
+    // TODO: Make a dedicated Phasor struct
+    freq: f64,
+    angle_delta: f64,
+    current_angle: f64,
 }
 
 impl ElysiumAudioProcessor {
@@ -17,13 +22,15 @@ impl ElysiumAudioProcessor {
         Self {
             rng: rand::thread_rng(),
             sample_rate: DEFAULT_SAMPLE_RATE,
+            freq: DEFAULT_FREQUENCY,
+            angle_delta: 0.0,
+            current_angle: 0.0,
         }
     }
 
     pub fn prepare_to_play(&mut self, sample_rate: f64, _maximum_expected_samples_per_block: i32) {
-        if sample_rate > 0.0 {
-            self.sample_rate = sample_rate;
-        }
+        self.sample_rate = sample_rate.max(0.0);
+        self.angle_delta = (self.freq / self.sample_rate) * 2.0 * std::f64::consts::PI;
     }
 
     pub fn process_block(&mut self, buf: Pin<&mut ffi::AudioBufferF32>) {
@@ -47,12 +54,15 @@ impl ElysiumAudioProcessor {
 
         let raw_array = buf.get_array_of_write_pointers();
 
-        for i in 0..channels {
-            unsafe {
-                let raw_channel = *raw_array.add(i);
-                for j in 0..samples {
-                    let sample = (self.rng.gen::<f32>() * 2.0) - 1.0;
-                    ptr::write(raw_channel.add(j), sample * 0.01);
+        for j in 0..samples {
+            let white_noise = (self.rng.gen::<f64>() * 2.0) - 1.0;
+            let sine_sample = self.current_angle.sin();
+            self.current_angle += self.angle_delta;
+
+            for i in 0..channels {
+                unsafe {
+                    let channel = *raw_array.add(i);
+                    ptr::write(channel.add(j), ((white_noise + sine_sample) * 0.01) as f32);
                 }
             }
         }
