bd5eef5483493e2baf7bcdfe0df8f4b1de4476d6
bepzi <bepzi@bepzi.com>
2021-04-24 20:10:28 -0700

    Replace homemade phasor with dasp

--- src/rust/src/processor.rs
+++ src/rust/src/processor.rs
@@ -1,39 +1,37 @@
 use core::pin::Pin;
-use rand::rngs::ThreadRng;
-use rand::Rng;
 use std::convert::TryFrom;
+
+use dasp_sample::Sample;
+use dasp_signal::Signal;
 
 use crate::ffi;
 
 const DEFAULT_SAMPLE_RATE: f64 = 41000.0;
-const DEFAULT_FREQUENCY: f64 = 440.0;
-const TWO_PI: f64 = std::f64::consts::PI * 2.0;
+const DEFAULT_FREQUENCY: f64 = 220.0;
 
 pub struct ElysiumAudioProcessor {
-    rng: ThreadRng,
     sample_rate: f64,
-
-    // TODO: Make a dedicated Phasor state struct
     freq: f64,
-    angle_delta: f64,
-    current_angle: f64,
+    signal: Option<Box<dyn Signal<Frame = f64>>>,
 }
 
 impl ElysiumAudioProcessor {
     pub fn new() -> Self {
         Self {
-            rng: rand::thread_rng(),
             sample_rate: DEFAULT_SAMPLE_RATE,
             freq: DEFAULT_FREQUENCY,
-            angle_delta: 0.0,
-            current_angle: 0.0,
+            signal: None,
         }
     }
 
     // Will be called on the main thread.
     pub fn prepare_to_play(&mut self, sample_rate: f64, _maximum_expected_samples_per_block: i32) {
         self.sample_rate = sample_rate.max(0.0);
-        self.angle_delta = (self.freq / self.sample_rate) * TWO_PI;
+        self.signal = Some(Box::new(
+            dasp_signal::rate(self.sample_rate)
+                .const_hz(self.freq)
+                .square(),
+        ));
     }
 
     // Will be called on the audio thread.
@@ -51,15 +49,13 @@ impl ElysiumAudioProcessor {
         }
 
         assert!(!audio.is_empty());
+        assert!(self.signal.is_some());
 
-        let samples = (0..audio[0].len())
-            .map(|_| {
-                let white_noise = (self.rng.gen::<f64>() * 2.0) - 1.0;
-                let sine_sample = self.current_angle.sin();
-                self.current_angle += self.angle_delta;
-                ((white_noise + sine_sample) * 0.01) as f32
-            })
-            .collect::<Vec<f32>>();
+        let signal = self.signal.as_mut().unwrap();
+        let samples = (0..audio[0].len()).map(|_| {
+            let s = signal.next() * 0.01;
+            s.to_sample::<f32>()
+        }).collect::<Vec<f32>>();
 
         for channel in audio.iter_mut() {
             channel.copy_from_slice(&samples);
