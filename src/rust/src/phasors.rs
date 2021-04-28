9516ecbc2d1385e5ac4df2f825f1c6261a43ded1
bepzi <bepzi@bepzi.com>
2021-04-28 13:00:18 -0700

    Distinguish between Phasors and Voices

--- src/rust/src/phasors.rs
+++ src/rust/src/phasors.rs
@@ -1,4 +1,14 @@
-const TWO_PI: f64 = 2.0 * std::f64::consts::PI;
+use wmidi::{Channel, Note, Velocity, U7};
+
+/// Alias for a played MIDI note.
+#[derive(Debug, Copy, Clone)]
+pub struct MidiNote {
+    pub channel: Channel,
+    pub note: Note,
+    pub velocity: Velocity,
+}
+
+// =============================================================================
 
 /// Semantic alias for frequency.
 pub type Frequency = f64;
@@ -13,21 +23,22 @@ pub type Frequency = f64;
 /// # Examples
 ///
 /// ```
-/// # use crate::elysium::processor::phasor::Phasor;
+/// # use crate::elysium::phasors::Phasor;
 /// // A phasor wraps around once it reaches 1.0
 /// let mut phasor = Phasor::new(2.0, 1.0);
-/// assert_eq!(phasor.next(), Some(0.5));
 /// assert_eq!(phasor.next(), Some(0.0));
 /// assert_eq!(phasor.next(), Some(0.5));
+/// assert_eq!(phasor.next(), Some(0.0));
 ///
 /// // Create a phasor for audio sampling rates and frequencies
 /// let mut phasor = Phasor::new(44100.0, 440.0);
 /// let val = phasor.next().unwrap();
-/// assert_eq!(val, 0.009977324263038548);
+/// assert_eq!(val, 0.0);
+/// assert_eq!(phasor.next(), Some(0.009977324263038548));
 ///
 /// // Shift from [0, 1) to [0, 2pi), then take the sine
 /// let val = (val * 2.0 * std::f64::consts::PI).sin();
-/// assert_eq!(val, 0.06264832417874368);
+/// assert_eq!(val, 0.0);
 /// ```
 ///
 /// [phasor]: https://en.wikipedia.org/wiki/Phasor
@@ -37,12 +48,6 @@ pub struct Phasor {
     freq: Frequency,
     phase: f64,
     phase_incr: f64,
-}
-
-impl Default for Phasor {
-    fn default() -> Self {
-        Self::new(44100.0, 440.0)
-    }
 }
 
 impl Phasor {
@@ -59,8 +64,8 @@ impl Phasor {
     /// # Panics
     ///
     /// This function will panic if `sample_rate` is `<= 0.0` or
-    /// `freq` is `< 0.0`, or if either are infinity or
-    /// `NaN`. `freq` must be less than or equal to `sample_rate`.
+    /// `freq` is `< 0.0`, or if either are infinity or `NaN`. `freq`
+    /// must also be less than or equal to `sample_rate`.
     pub fn new(sample_rate: Frequency, freq: Frequency) -> Self {
         let mut phasor = Self {
             sample_rate,
@@ -72,9 +77,16 @@ impl Phasor {
         phasor
     }
 
+    /// Resets the phasor's position to its initial value of `0.0`.
+    ///
+    /// This does not affect its current sampling rate or frequency.
+    pub fn reset(&mut self) {
+        self.phase = 0.0;
+    }
+
     /// Gets the current sampling rate.
-    pub fn get_sample_rate(&self) -> f64 {
-        return self.sample_rate;
+    pub fn get_sample_rate(&self) -> Frequency {
+        self.sample_rate
     }
 
     /// Changes the sampling rate, but not the frequency.
@@ -109,8 +121,8 @@ impl Phasor {
     }
 
     /// Gets the current frequency.
-    pub fn get_freq(&self) -> f64 {
-        return self.freq;
+    pub fn get_freq(&self) -> Frequency {
+        self.freq
     }
 }
 
@@ -118,44 +130,69 @@ impl Iterator for Phasor {
     type Item = f64;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let value = self.phase;
         self.phase += self.phase_incr;
         if self.phase >= 1.0 {
             self.phase -= 1.0;
         }
-        Some(self.phase)
+        Some(value)
     }
 }
 
-pub struct FnMutPhasor {
-    pub phasor: Phasor,
-    pub fn_mut: Box<dyn FnMut(f64) -> f64>,
+// =============================================================================
+
+#[derive(Debug, Copy, Clone)]
+pub struct Voice {
+    phasor: Phasor,
+    last_played_at: std::time::Instant,
+    note: Option<MidiNote>,
+    velocity: f64,
 }
 
-impl Iterator for FnMutPhasor {
-    type Item = f64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        Some((self.fn_mut)(self.phasor.next().unwrap()))
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct SinePhasor {
-    pub phasor: Phasor,
-}
-
-impl Default for SinePhasor {
-    fn default() -> Self {
+impl Voice {
+    pub fn new(sample_rate: Frequency) -> Self {
         Self {
-            phasor: Phasor::default(),
+            phasor: Phasor::new(sample_rate, 0.0),
+            last_played_at: std::time::Instant::now(),
+            note: None,
+            velocity: 0.0,
         }
     }
+
+    pub fn start_playing(&mut self, note: MidiNote) {
+        self.phasor.reset();
+        self.phasor.set_freq(note.note.to_freq_f64());
+        self.last_played_at = std::time::Instant::now();
+        self.note = Some(note);
+        self.velocity = u8::from(note.velocity) as f64 / u8::from(U7::MAX) as f64
+    }
+
+    pub fn stop_playing(&mut self) {
+        self.note = None;
+    }
+
+    pub fn currently_playing(&self) -> Option<MidiNote> {
+        self.note
+    }
+
+    pub fn last_played_at(&self) -> std::time::Instant {
+        self.last_played_at
+    }
 }
 
-impl Iterator for SinePhasor {
+impl Iterator for Voice {
     type Item = f64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some((self.phasor.next().unwrap() * TWO_PI).sin())
+        if self.note.is_some() {
+            // TODO: Obviously this should be more interesting than
+            // just a sine wave. How should we handle things like
+            // realtime parameters and complex waveforms?
+            let value = self.phasor.next().unwrap();
+            let value = (value * 2.0 * std::f64::consts::PI).sin();
+            Some(value * self.velocity)
+        } else {
+            Some(0.0)
+        }
     }
 }
--- src/rust/src/processor.rs
+++ src/rust/src/processor.rs
@@ -2,17 +2,18 @@ use core::pin::Pin;
 use std::collections::{HashMap, HashSet};
 use std::convert::TryFrom;
 
-use wmidi::{MidiMessage, Note, Velocity};
+use wmidi::{Channel, MidiMessage, Note, Velocity};
 
 use crate::ffi;
-use crate::phasors::SinePhasor;
+use crate::phasors::{MidiNote, Voice};
 
 const DEFAULT_SAMPLE_RATE: f64 = 41000.0;
+const MAX_NUM_VOICES: usize = 16;
 
 pub struct ElysiumAudioProcessor {
     sample_rate: f64,
     // TODO: Use a datastructure that doesn't allocate
-    voices: HashMap<Note, SinePhasor>,
+    voices: Vec<Voice>,
     midi_state: MidiState,
 }
 
@@ -20,7 +21,7 @@ impl Default for ElysiumAudioProcessor {
     fn default() -> Self {
         Self {
             sample_rate: DEFAULT_SAMPLE_RATE,
-            voices: HashMap::new(),
+            voices: Vec::new(),
             midi_state: MidiState::new(),
         }
     }
@@ -30,7 +31,7 @@ impl ElysiumAudioProcessor {
     // Will be called on the main thread.
     pub fn prepare_to_play(&mut self, sample_rate: f64, _maximum_expected_samples_per_block: i32) {
         self.sample_rate = sample_rate.max(0.0);
-        self.voices = HashMap::new();
+        self.voices = vec![Voice::new(self.sample_rate); MAX_NUM_VOICES];
         self.midi_state = MidiState::new();
     }
 
@@ -48,31 +49,71 @@ impl ElysiumAudioProcessor {
         self.midi_state.process_midi_messages(midi);
 
         for off in &self.midi_state.notes_turned_off {
-            self.voices.remove(off);
+            for voice in &mut self.voices {
+                if let Some(note) = voice.currently_playing() {
+                    if note.note == *off {
+                        voice.stop_playing();
+                        break;
+                    }
+                }
+            }
         }
 
-        for (on, _velocity) in &self.midi_state.notes_turned_on {
-            self.voices.insert(
-                *on,
-                // TODO: Don't heap allocate for each NoteOn event
-                {
-                    let mut s = SinePhasor::default();
-                    s.phasor.set_sample_rate(self.sample_rate);
-                    s.phasor.set_freq(on.to_freq_f64());
-                    s
-                },
-            );
+        for (on, velocity) in &self.midi_state.notes_turned_on {
+            let note = MidiNote {
+                channel: Channel::Ch1,
+                note: *on,
+                velocity: *velocity,
+            };
+
+            let mut found_unused_voice = false;
+            for voice in &mut self.voices {
+                if voice.currently_playing().is_none() {
+                    voice.start_playing(note);
+                    found_unused_voice = true;
+                    break;
+                }
+            }
+
+            if !found_unused_voice {
+                // TODO: What I really wanted to do was store a
+                // mutable reference so that I could just break from
+                // the loop and immediately call
+                // start_playing(). Fortunately, this code is the slow
+                // path, so I don't think it matters.
+                let mut least_recently_used = std::time::Instant::now();
+                for voice in &self.voices {
+                    if voice.last_played_at() < least_recently_used {
+                        least_recently_used = voice.last_played_at();
+                    }
+                }
+
+                for voice in &mut self.voices {
+                    if voice.last_played_at() == least_recently_used {
+                        voice.start_playing(note);
+                        found_unused_voice = true;
+                        break;
+                    }
+                }
+            }
+
+            assert!(found_unused_voice)
         }
 
         // Pull a single frame's worth of samples from each active
         // voice, then sum them together to get the final list of
         // samples.
+
+        // TODO: Can't I just reduce the voice iterators down? (Maybe
+        // I need to make the voices stereo first, to handle that
+        // usecase).
         let mut equilibrium = vec![0.0f64; audio[0].len()];
 
         let voice_sample_iters = self
             .voices
-            .values_mut()
-            .map(|voice| (0..audio[0].len()).map(move |_| voice.next().unwrap() * 0.02));
+            .iter_mut()
+            .filter(|voice| voice.currently_playing().is_some())
+            .map(|voice| (0..audio[0].len()).map(move |_| voice.next().unwrap()));
 
         for voice_iter in voice_sample_iters {
             for (i, sample) in voice_iter.enumerate() {
@@ -80,7 +121,10 @@ impl ElysiumAudioProcessor {
             }
         }
 
-        let samples: Vec<f32> = equilibrium.into_iter().map(|f| f as f32).collect();
+        let samples: Vec<f32> = equilibrium
+            .into_iter()
+            .map(|f| (f * 0.075) as f32)
+            .collect();
         for channel in audio.iter_mut() {
             channel.copy_from_slice(&samples);
         }
@@ -90,16 +134,23 @@ impl ElysiumAudioProcessor {
 #[derive(Debug)]
 struct MidiState {
     // TODO: Use a datastructure that doesn't allocate
+    //
+    // TODO: Need to take MIDI channels into account too. Should
+    // probably just store entire MidiNote structs.
     notes_turned_on: HashMap<Note, Velocity>,
     notes_turned_off: HashSet<Note>,
 }
 
 impl MidiState {
     fn new() -> Self {
-        Self {
+        let mut s = Self {
             notes_turned_on: HashMap::new(),
             notes_turned_off: HashSet::new(),
-        }
+        };
+
+        s.notes_turned_on.reserve(128);
+        s.notes_turned_off.reserve(128);
+        s
     }
 
     fn process_midi_messages(&mut self, mut midi: Pin<&mut ffi::MidiBufferIterator>) {
